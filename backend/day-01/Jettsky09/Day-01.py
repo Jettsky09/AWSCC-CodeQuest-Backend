59e98bb70a43a4da761fb25a7dd2314dc390ef14
Jeff Petterson Bernandino Mercado <141805107+Jettsky09@users.noreply.github.com>
2023-11-21 07:47:57 +0800

    Updated Day-01

--- backend/day-01/Jettsky09/Day-01.py
+++ backend/day-01/Jettsky09/Day-01.py
@@ -5,6 +5,6 @@ program = "Computer Science"
 provice = "Nueva Ecija"
 department = "Department of Software Development"
 
-print(f"\nHello! I'm {name},{age} years old. I'm from the province of {provice}.\
+print(f"\nHello! I'm {name}, {age} years old. I'm from the province of {provice}.\
     \nI'm taking my {program} program in {school}. Which is a home for a bunch of cool students like me(just kidding).\
     \nAnd because I'm cool I'm gonna finish the {department}'s Codequest.\n")
