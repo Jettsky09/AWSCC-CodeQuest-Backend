a406e77c9257d0a448600283c8ef61e6c5634c7f
Jeff Petterson Bernandino Mercado <141805107+Jettsky09@users.noreply.github.com>
2023-11-20 20:09:46 +0800

    Day-01
    Introduction of myself using the basics of python

---
+++ backend/day-01/Jettsky09/Day-01.py
@@ -0,0 +1,10 @@
+name = "Jeff Petterson B. Mercado"
+age = 18 
+school = "Polytechnic University of the Philippines"
+program = "Computer Science"
+provice = "Nueva Ecija"
+department = "Department of Software Development"
+
+print(f"\nHello! I'm {name},{age} years old. I'm from the province of {provice}.\
+    \nI'm taking my {program} program in {school}. Which is a home for a bunch of cool students like me(just kidding).\
+    \nAnd because I'm cool I'm gonna finish the {department}'s Codequest.\n")
