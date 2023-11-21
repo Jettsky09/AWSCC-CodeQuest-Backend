d0fadbf39462a5257631b0f8f0fdee25e6904f3e
Jeff Petterson Bernandino Mercado <141805107+Jettsky09@users.noreply.github.com>
2023-11-21 09:08:59 +0800

    day-03

---
+++ backend/day-03/day-3.py
@@ -0,0 +1,26 @@
+print(f"````````````\n")
+name = input("Enter your name: ")
+print(f"\nLong time ago there is a man goes by the name {name}. {name} is strongest man that been alive.\
+      \nSadly after he defeated Mayweather and friends, he suddenly disappered.\
+      \nToday, the humanity's darkness is back, so is {name}\n")
+
+print(f"Controls: 1 = Affirmation, 0 = Negation\n")
+
+print(f"{name}!!! There is an ongoing robbery in the Puregold Jr Pureza!!!\
+    \n -- Don't worry I'm otw there(PRESS 1)\
+    \n -- Tulog muna ako men, tayup ako eh(PRESS 0)\n")
+      
+control = int(input('What are you going to do?? '))
+if control == 1:
+    print(f"\n{name} again saves the day!\
+         \n Press any key to proceed to next level")
+else:
+    print(f"\nPureza was devoured by darkness :(((\
+        \n       GAME OVER, TRY AGAIN")
+print(f"````````````\n")
+
+
+
+
+
+
