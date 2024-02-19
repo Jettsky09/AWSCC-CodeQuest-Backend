4651007bdcee9b760dc869ba2df37911c6ad12b1
Jeff Petterson Bernandino Mercado <141805107+Jettsky09@users.noreply.github.com>
2024-02-19 19:12:18 +0800

    Utilization of code
    aesthetics of code

---
+++ avg.py
@@ -0,0 +1,13 @@
+def avg(numlist):
+    sum = 0
+    avg = 0
+
+    for num in numlist:
+        sum += num
+
+    avg = sum / len(numlist)
+    return avg 
+
+my_list = [5, 9, 2, 3, 1]
+my_avg = avg(my_list)
+print(my_avg)
--- submissions/mini-project-01/jettsky/shop_list.py
+++ submissions/mini-project-01/jettsky/shop_list.py
@@ -1,5 +1,7 @@
 shop_list = []
 i=0
+Length = len(shop_list)
+
 while i != -1:
     print(f"\nOptions:\
 \n1. Add item to the shopping list\
@@ -8,18 +10,17 @@ while i != -1:
 \n4. Quit")
     name = (int(input('Enter the number of your choice: ')))
     if name == 1:
-            shop_list.append(input("Enter the item you want to add: "))
-            Length = len(shop_list )
-            print(shop_list [Length-1]+" has been added to your shopping list.")
-    if name == 2:
+            add_list = input("Enter the item you want to add: ")
+            shop_list.append(add_list)
+            print(shop_list[Length-1]  +" has been added to your shopping list.")
+    elif name == 2:
             for item in shop_list :
                 print(item)
-    if name == 3:
+    elif name == 3:
             remove_list = input("Enter the item you want to remove: ")
-            Length = len(shop_list)
-            print(shop_list [Length -1]+" has been removed from your shopping list.")
+            print(remove_list +" has been removed from your shopping list.")
             shop_list.remove(remove_list)
-    if name == 4:
+    elif name == 4:
             print("Good Bye!\n")
             break
     i+=1
