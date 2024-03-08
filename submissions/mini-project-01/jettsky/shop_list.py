6ec5e29c710f4aec44dc729f50ed1ddbcd722d85
Jeff Petterson Bernandino Mercado <141805107+Jettsky09@users.noreply.github.com>
2024-03-08 11:33:41 +0800

    lower caps input

--- submissions/mini-project-01/jettsky/shop_list.py
+++ submissions/mini-project-01/jettsky/shop_list.py
@@ -10,14 +10,14 @@ while i != -1:
 \n4. Quit")
     name = (int(input('Enter the number of your choice: ')))
     if name == 1:
-            add_list = input("Enter the item you want to add: ")
+            add_list = input("Enter the item you want to add: ").lower()
             shop_list.append(add_list)
             print(shop_list[Length-1]  +" has been added to your shopping list.")
     elif name == 2:
             for item in shop_list :
                 print(item)
     elif name == 3:
-            remove_list = input("Enter the item you want to remove: ")
+            remove_list = input("Enter the item you want to remove: ").lower()
             print(remove_list +" has been removed from your shopping list.")
             shop_list.remove(remove_list)
     elif name == 4:
